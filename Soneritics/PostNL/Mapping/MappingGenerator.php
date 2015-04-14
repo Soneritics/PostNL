8c2c18a78077b3eb849c4fc20971baf77895c25e
Jordi Jolink <info@soneritics.nl>
2015-04-14 22:36:43 +0200

    Juiste copyrightvermelding.

--- Soneritics/PostNL/Mapping/MappingGenerator.php
+++ Soneritics/PostNL/Mapping/MappingGenerator.php
@@ -2,7 +2,7 @@
 /* 
  * The MIT License
  *
- * Copyright 2014 Soneritics Webdevelopment.
+ * Copyright 2015 Soneritics Webdevelopment.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
